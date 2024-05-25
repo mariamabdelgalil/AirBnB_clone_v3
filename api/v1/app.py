ec6f05fa42aa14dafac8f809093dfd65b269299b
Mariam Abdelgalil <mariam.ahmad.200300@gmail.com>
2024-05-25 07:53:23 -0700

    task 3 infrastructure

---
+++ api/v1/app.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+starts a Flask web application
+"""
+from flask import Flask
+from models import storage
+from api.v1.views import app_views
+HOST = '0.0.0.0'
+PORT = '5000'
+
+app = Flask(__name__)
+app.register_blueprint(app_views)
+
+@app.teardown_appcontext
+def teardown_appcontext(code):
+    """Close storage in when flask app finish its process"""
+    storage.close()
+
+if __name__ == "__main__":
+    app.run(host=HOST, port=PORT)
---
+++ api/v1/views/index.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Views index module"""
+from api.v1.views import app_views
+from flask import jsonify
+
+@app_views.route('/status', strict_slashes=False)
+def status_route():
+    """status route function: return OK"""
+    return jsonify({'status': 'OK'})
