87b0164c20abad2d33672a7a67455e49a4d653b5
KhaledAbdelgalil <khaled.abdelgalil96@gmail.com>
2024-05-26 00:56:28 +0100

    adjust task 6

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,14 +1,11 @@
 #!/usr/bin/python3
-"""
-starts a Flask web application
-"""
-from flask import Flask, make_response, jsonify
+"""The app module"""
+from flask import Flask, jsonify, make_response, request, abort
 from models import storage
 from api.v1.views import app_views
 from os import getenv
 HOST = getenv("HBNB_API_HOST") or '0.0.0.0'
 PORT = getenv("HBNB_API_PORT") or '5000'
-
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
@@ -26,10 +23,11 @@ def not_found(e):
     return make_response(jsonify({'error': 'Not found'}), 404)
 
 
-@app.errorhandler(404)
-def not_found(e):
-    """Handle 404 not found page"""
-    return make_response(jsonify({'error': 'Not found'}), 404)
+@app.before_request
+def check_content_type():
+    """Check if the request content type is not JSON"""
+    if request.content_type and request.content_type != 'application/json':
+        abort(400, description="Content-Type must be application/json")
 
 
 if __name__ == "__main__":
--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -1,41 +1,26 @@
 #!/usr/bin/python3
-"""Views index module"""
+"""The states module"""
 from api.v1.views import app_views
-from flask import jsonify, make_response, request, abort
+from flask import abort, jsonify, make_response, request
 from models import storage
 from models.state import State
 
 
 @app_views.route('/states', methods=['GET'], strict_slashes=False)
 def get_states():
-    """states route function"""
-    states = storage.all(State).values()
-    states_arr = []
-    for state in states:
-        states_arr.append(state.to_dict())
-    return jsonify(states_arr)
+    """return all states information"""
+    states = [state.to_dict() for state in storage.all(State).values()]
+    return jsonify(states)
 
 
 @app_views.route('/states/<string:state_id>', methods=['GET'],
                  strict_slashes=False)
 def get_state(state_id):
-    """state route function"""
+    """return information about a state by id"""
     state = storage.get(State, state_id)
     if state is None:
         abort(404)
     return jsonify(state.to_dict())
-
-
-@app_views.route('/states', methods=['POST'], strict_slashes=False)
-def post_state():
-    """post state route function"""
-    if not request.get_json():
-        return make_response(jsonify({'error': 'Not a JSON'}), 400)
-    if 'name' not in request.get_json():
-        return make_response(jsonify({'error': 'Missing name'}), 400)
-    state = State(**request.get_json())
-    state.save()
-    return make_response(jsonify(state.to_dict()), 201)
 
 
 @app_views.route('/states/<string:state_id>', methods=['DELETE'],
@@ -50,16 +35,30 @@ def delete_state(state_id):
     return jsonify({})
 
 
+@app_views.route('/states/', methods=['POST'], strict_slashes=False)
+def post_state():
+    """create a new state"""
+    json_body = request.get_json()
+    if not json_body:
+        abort(400, "Not a JSON")
+    if 'name' not in json_body:
+        abort(400, "Missing name")
+    new_state = State(**json_body)
+    new_state.save()
+    return make_response(jsonify(new_state.to_dict()), 201)
+
+
 @app_views.route('/states/<string:state_id>', methods=['PUT'],
                  strict_slashes=False)
 def put_state(state_id):
-    """update a state"""
+    """update an state"""
     state = storage.get(State, state_id)
+    json_body = request.get_json()
     if not state:
         abort(404)
-    if not request.get_json():
+    if not json_body:
         abort(400, "Not a JSON")
-    for key, value in request.get_json().items():
+    for key, value in json_body.items():
         if key not in ['id', 'created_at', 'updated_at']:
             setattr(state, key, value)
     state.save()
