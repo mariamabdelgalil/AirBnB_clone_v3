41b581ea7c8fc45d7d270ee2dbe78079eb492b89
Mariam Abdelgalil <mariam.ahmad.200300@gmail.com>
2024-05-25 07:57:30 -0700

    task 3 infrastructure styling

--- api/v1/app.py
+++ api/v1/app.py
@@ -8,13 +8,16 @@ from api.v1.views import app_views
 HOST = '0.0.0.0'
 PORT = '5000'
 
+
 app = Flask(__name__)
 app.register_blueprint(app_views)
+
 
 @app.teardown_appcontext
 def teardown_appcontext(code):
     """Close storage in when flask app finish its process"""
     storage.close()
+
 
 if __name__ == "__main__":
     app.run(host=HOST, port=PORT)
